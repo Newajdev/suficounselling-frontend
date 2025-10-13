f6c9af442ef5a0caf9daef9a77c8f702b8caf825
newajDev <newaj.design@gmail.com>
2025-10-14 00:07:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Runnner from './Runnner'
+
+function App() {
+  
+
+  return (
+    <>
+      <Runnner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Runnner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Runnner() {
+    return (
+        <div className="max-w-[1240px] mx-auto gap-6 h-screen flex justify-center items-center">
+            <img src="./public/Logo.png" alt="" />
+            <div>
+                <h1 className="text-8xl font-black text-[#224047]">DIRI Sufi Counselling Center</h1>
+                <p className="text-red-600 font-bold text-2xl mt-8">This website is under Construction <br />Please Stay with us we
+                    will live soon</p>
+                <div className="text-2xl font-black mt-5">
+                    <p>For any info:</p>
+                    <p>01711-817274, 01841-444433</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Runnner
